f14bd6591b8b620dbfcd7be8fdb6dcdf2cbb585c
Jinsol Kim <jinsol032360@gmail.com>
2024-01-19 17:01:32 +0900

    :feat: 글 발행 기능

--- forum/app/write/page.js
+++ forum/app/write/page.js
@@ -2,7 +2,9 @@ export default function Write() {
     return (
         <div>
             <h4>글작성</h4>
-            <form action="/api/list" method="GET">
+            <form action="/api/list" method="POST">
+                <input name="title" placeholder="제목을 입력해주세요" />
+                <input name="content" placeholder="내용을 입력해주세요" />
                 <button type="submit">버튼</button>
             </form>
         </div>
--- forum/pages/api/list.js
+++ forum/pages/api/list.js
@@ -1,13 +1,30 @@
 import { connectDB } from '@/util/database';
 
 export default async function handler(req, res) {
-    const db = (await connectDB).db('forum');
-    let result = await db.collection('post').find().toArray();
+    if (req.method === 'POST') {
+        try {
+            const { title, content } = req.body;
+            console.log(req.body);
 
-    if (req.method === 'GET') {
-        return res.status(200).json(result);
+            // 제목과 내용이 제공되었는지 확인
+            if (!title || !content) {
+                return res.status(400).json({ error: '제목과 내용은 필수 입력 사항입니다.' });
+            }
+
+            const db = (await connectDB).db('forum');
+
+            // 데이터베이스에 새로운 글 추가
+            await db.collection('post').insertOne({
+                title: title,
+                content: content,
+            });
+
+            return res.status(200).json('글 등록 성공');
+        } catch (error) {
+            console.error('글 등록 실패 :', error);
+            return res.status(500).json('글 등록에 실패했습니다.');
+        }
     } else {
-        return res.status(400).json('처리실패');
+        return res.status(405).json({ error: '메서드는 POST만 허용됩니다.' });
     }
-    // return res.status(200).json('처리완료');
 }
