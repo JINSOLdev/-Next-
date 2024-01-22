13f83fb46e51424a84b213f67860632fedb9188a
Jinsol Kim <jinsol032360@gmail.com>
2024-01-22 15:28:05 +0900

    💡 Ajax 에러 처리

--- forum/app/list/ListItem.js
+++ forum/app/list/ListItem.js
@@ -18,9 +18,17 @@ export default function ListItem({ result }) {
                                     body: result[i]._id,
                                 })
                                     .then((r) => {
-                                        return r.json();
+                                        if (r.status === 200) {
+                                            return r.json();
+                                        } else {
+                                            // 서버가 에러코드 전송 시 실행할 코드
+                                        }
+                                    })
+                                    .then((result) => {
+                                        // 성공 시 실행할 코드
                                     })
                                     .catch((error) => {
+                                        // 인터넷 문제로 실패 시 실행할 코드
                                         console.log(error);
                                     });
                             }}
