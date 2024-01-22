879e1e63a970c94e72db7b69d5fbabc30866d89a
Jinsol Kim <jinsol032360@gmail.com>
2024-01-22 09:33:55 +0900

    🩹 회원가입 이름 아이디 중복, 비밀번호 일치 여부

--- forum/pages/api/post/member_new.js
+++ forum/pages/api/post/member_new.js
@@ -11,13 +11,24 @@ export default async function handler(req, res) {
 
             const db = (await connectDB).db('forum');
 
-            await db.collection('signup').insertOne({
-                name: name,
-                id: id,
-                password1: password1,
-                password2: password2,
-            });
+            // 기존 사용자 목록 조회
+            const existingName = await db.collection('signup').findOne({ name });
+            const existingId = await db.collection('signup').findOne({ id });
 
+            // 중복체크
+            if (existingName) {
+                return res.status(400).json({ error: '이미 존재하는 이름 입니다.' });
+            } else if (existingId) {
+                return res.status(400).json({ error: '이미 존재하는 아이디 입니다.' });
+            }
+
+            // 비밀번호 일치 여부 확인
+            if (password1 !== password2) {
+                return res.status(400).json({ error: '비밀번호가 일치하지 않습니다.' });
+            }
+
+            // 회원가입 성공 시 데이터베이스에 저장
+            await db.collection('signup').insertOne({ name, id, password1, password2 });
             return res.status(200).json('회원가입 성공');
         } catch (error) {
             console.error('회원가입 실패 :', error);
