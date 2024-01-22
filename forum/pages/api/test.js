39c2c5848cb306eff618dbb8b42b503ad36744a0
Jinsol Kim <jinsol032360@gmail.com>
2024-01-22 15:44:00 +0900

    💡 query string, URL parameter를 활용한 삭제 기능

--- forum/pages/api/test.js
+++ forum/pages/api/test.js
@@ -1,8 +1,4 @@
 export default function handler(req, res) {
-    if (req.method === 'GET') {
-        return res.status(200).json('처리완료');
-    } else {
-        return res.status(400).json('처리실패');
-    }
-    // return res.status(200).json('처리완료');
+    console.log(req.query);
+    return res.status(200).json('처리완료');
 }
